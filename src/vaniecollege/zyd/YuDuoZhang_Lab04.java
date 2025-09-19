fd50f44786c2c5d68e5f586ec156acffd7ade388
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-09-19 13:21:43 -0400

    Element Creation

--- src/vaniecollege/zyd/YuDuoZhang_Lab04.java
+++ src/vaniecollege/zyd/YuDuoZhang_Lab04.java
@@ -1,20 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package vaniecollege.zyd;
 
+import javafx.application.Application;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
 /**
- *
- * @author CnFig
+ * @author Yu Duo Zhang (2480549)
+ * https://github.com/zyd-074/Vanier_F25_420-SF3-RE_Lab04.git
  */
-public class YuDuoZhang_Lab04 {
+public class YuDuoZhang_Lab04 extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        GridPane root = new GridPane();
+        
+        Label tripDays = new Label("Days on the trip: ");
+        Label airfare = new Label("Amount of airfare: ");
+        Label carRental = new Label("Amount of car rental fees: ");
+        Label milesDriven = new Label("Number of miles driven: ");
+        Label parking = new Label("Amount of parking fees: ");
+        Label taxi = new Label("Amount of txi charges: ");
+        Label registration = new Label("Conference or seminar registration fees: ");
+        Label lodging = new Label("Lodging charges (per night): ");
+        
+        TextField userTipDays = new TextField("Required");
+        TextField userAirfare = new TextField("Optional");
+        TextField userCarRental = new TextField("Optional");
+        TextField userMilesDriven = new TextField("If private vehicle was used");
+        TextField userParking = new TextField("Optional");
+        TextField userTaxi = new TextField("Optional");
+        TextField userRegistration = new TextField("Optional");
+        TextField userLodging = new TextField("Per night");
     }
     
 }
